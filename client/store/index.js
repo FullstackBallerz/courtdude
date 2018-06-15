8f09a110580f7e1784d4041d9c7108099be1d2f4
DK <dkdan94@gmail.com>
2018-06-15 17:41:23 -0400

    store file

---
+++ client/store/index.js
@@ -0,0 +1,6 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import createLogger from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const reducer = combineReducer({});
