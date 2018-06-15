180edad6bf9c636f7adce76ff6bde5cce3cb948f
Sean Lee <seanslee92@gmail.com>
2018-06-15 17:40:36 -0400

    added basic routing

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const session = require('express-session');
-const passport = require('passport');
-const db = require('./db');
-const sessionStore = new SequelizeStore({ db });
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 const app = express();
 
 module.exports = app;
+
+const createApp = () => {
+    // logging middleware
+    app.use(morgan('dev'))
+  
+    // body parsing middleware
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.listen(PORT, () =>
+      console.log(`Mixing it up on port ${PORT}`)
+    )
+    app.use('/api', require('./api'))
+  }
+
+createApp()
+
