0bb09002d044431e26a2b3f731526ecc7bbeef58
Sean Lee <seanslee92@gmail.com>
2018-06-14 22:42:08 -0400

    merge conflict

--- App.js
+++ App.js
@@ -5,28 +5,6 @@ import { MapView } from 'expo';
 export default class App extends React.Component {
   render() {
     return (
-<<<<<<< HEAD
-  <MapView
-    initialRegion={{
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }}
-  />
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-=======
       <MapView
         style={{
           flex: 1
@@ -41,4 +19,3 @@ const styles = StyleSheet.create({
     );
   }
 }
->>>>>>> 653abe0d67a41e5fec31851157d01fb74cc937d9
