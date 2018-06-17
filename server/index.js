71379e45213042921fa6071710b5ba3ec85d7ba6
DK <dkdan94@gmail.com>
2018-06-17 16:58:50 -0400

    resets back to initial setup

--- server/index.js
+++ server/index.js
@@ -7,17 +7,14 @@ const app = express();
 module.exports = app;
 
 const createApp = () => {
-    // logging middleware
-    app.use(morgan('dev'))
-  
-    // body parsing middleware
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({ extended: true }))
-    app.listen(PORT, () =>
-      console.log(`Mixing it up on port ${PORT}`)
-    )
-    app.use('/api', require('./api'))
-  }
+  // logging middleware
+  app.use(morgan('dev'));
 
-createApp()
+  // body parsing middleware
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
+  app.use('/api', require('./api'));
+};
 
+createApp();
